9686630f5df7e5c3cc7755499f156cac46c38965
Tanu5 <tammanatanusha@gmail.com>
2016-10-10 18:28:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,19 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='modified text';
+//move the image
+
+var img=document.getEelementById('madi');
+var marginleft=0;
+function moveRight()
+{
+    marginleft=marginleft +2;
+    img.style.marginleft=marginleft+px;
+}
+img.onclick=function()
+{
+  var interval=SetIneterval(moveright, 40);  
+};
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
