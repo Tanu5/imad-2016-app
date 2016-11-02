43e83ecdcd9f24698d5cdcea95e5ee58e32002aa
Tanu5 <tammanatanusha@gmail.com>
2016-11-03 00:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ var article={
         }
 };
 function createTemplate(data){
-var title= data.title,
-var date= data.date,
-var heading= data.heading,
-var content= data.content,
+var title= data.title;
+var date= data.date;
+var heading= data.heading;
+var content= data.content;
 var htmlTemplate= `
 <html>
   <head>
