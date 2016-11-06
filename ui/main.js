bc745d05999225c6635fb75a3d394c43f2f3b705
Tanu5 <tammanatanusha@gmail.com>
2016-11-06 23:30:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,23 @@
 //counter code
 var button=document.getElementById('counter');
-var counter = 0;
 
 button.onclick=function(){
     //make request to counter endpoint
-    
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
-    
-    //redner the variable in correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if(request.readystate ===XMLHttpRequest.DONE) {
+            //Take  some action
+            if(request.status ===200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    //make the request
+    request.open('GET', 'http://tanu5.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
+    
