23981bc175804e73074c51f751303f2f004311d6
Tanu5 <tammanatanusha@gmail.com>
2016-11-06 20:43:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,80 +4,14 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-//var articles={
-  //  'article-one':{
-    //    title:'Article One'| Tanusha,
-      //  heading: Articleone,
-        //date:'sep 5,2016',
-        //content:`
-          //  <p>
-            //      This is my first article and also first webapp.
-            //</p>`
-        //},
-    //'article-two':{
-      //  title:'Article Two'| Tanusha,
-        //heading: Articletwo,
-        //date:'sep 10,2016',
-        //content:`
-          //  <p>
-            //    This is my second article.
-            //</p>`
-        //},
-    //'article-three':{
-      //  title:'Article Three'| Tanusha,
-        //heading: Articlethree,
-        //date:'sep 15,2016',
-        //content:`
-          //  <p>
-            //    This is my third article.
-            //</p>`
-        //}
-//};
-//function createTemplate(data) {
-//var title= data.title;
-//var date= data.date;
-//var heading= data.heading;
-//var content= data.content;
-//var htmlTemplate= `
-//<html>
- // <head>
-   //  <title>
-     //   ${title}
-    //</title>
-    //<meta name="viewport" content="width-devide-width,intial scale-1"  />
-    //<link href="/ui/style.css" rel="stylesheet" />
-    //</head>
-      //     <body>
-        //      <div class="container">
-          //        <div>
-            //          < a href="/">Home</a>
-              //    </div>
-                //  </hr>
-                  // <h3>
-                    //      ${heading}
-                    //</h3>
-                    //<div>
-                      //     ${date}
-                    //</div>
-                     //<div>
-                       //    ${content}
-                      //</div>
-            //    </div>
-            //</body>
-  //</html>
-  //`;
-//return htmlTemplate;
-//}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
 });
-//app.get('/:articleName',function (req,res){
-    //articleName == article-one
-    //articles[articleName]== {} content object for article one
-  //  var articleName=req.params.articleName;
-   //res.sendFile(createTemplate(articles[articleName])); 
-//});
-
+var counter=0;
+app.get('/counter', function (req, res) {
+     counter=counter+1;
+   res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
