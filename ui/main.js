96bdae383f22945e3f8fe2405adc5bc7e68d31dd
Tanu5 <tammanatanusha@gmail.com>
2016-11-07 20:08:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function(){
     
     
     //make the request
-    request.open('GET', 'http://tanu5.imad.hasura-app.io/counter', true);
-    request.send(null);
+    request. open('GET', 'http://tanu5.imad.hasura-app.io/counter', true);
+    request. send(null);
 };
    
