c39ed8eb35b240663f45aec1109ff7b20744de3e
Tanu5 <tammanatanusha@gmail.com>
2016-11-12 20:09:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,9 @@ function createTemplate(data) {
     ` ;
         return htmlTemplate;
 }
+  app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
+});
 app.get('/articleName', function(req,res)
         {
             //articleName == article-one
@@ -81,9 +84,7 @@ app.get('/articleName', function(req,res)
             var articleName = req.params.articleName;
             res.send(createTemplate(articles[articleName]));
         });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
-});
+
 //var counter = 0;
 //app.get('/counter', function (req, res) {
   //   counter = counter + 1;
