5edf0acbccb905c852abfe134b07d9927680d5ee
Tanu5 <tammanatanusha@gmail.com>
2016-11-20 21:06:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,75 +12,75 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-//var articles = {
-  //   'article-one' : {
-    //     title : 'Article-One | Tanusha',
-      //   heading : 'Article-one',
-      //  date : 'sep 5,2016',
-         // content : `
-             //     <p>
-                    //     This is my first article
-                // </p>
-                // <p>
-                   //     Welcome to the first article
-                 // </p> `
-         // },
-     // 'article-two' : {
-        //   title : 'Article-Two | Tanusha',
-         // heading : 'Article-Two',
-         // date : 'sep 8,2016',
-         // content : `
-            //    <p>
-              //          This is my second article
-            //    </p> `
-         // },
-     // 'article-three' : {
-        // title : 'Article-Three | Tanusha',
-         // heading : 'Article-three',
-         // date : 'sep 15,2016',
-         // content : `
-            //     <p>
-                //         This is my third article
-                // </p>
-                // <p>
-                   //     this is the last  article
-                // </p> `
+var articles = {
+     'article-one' : {
+         title : 'Article-One | Tanusha',
+         heading : 'Article-one',
+        date : 'sep 5,2016',
+          content : `
+                  <p>
+                         This is my first article
+                </p>
+                 <p>
+                        Welcome to the first article
+                 </p> `
+          },
+     'article-two' : {
+          title : 'Article-Two | Tanusha',
+          heading : 'Article-Two',
+          date : 'sep 8,2016',
+          content : `
+                <p>
+                        This is my second article
+                </p> `
+          },
+      'article-three' : {
+         title : 'Article-Three | Tanusha',
+          heading : 'Article-three',
+          date : 'sep 15,2016',
+          content : `
+                 <p>
+                         This is my third article
+                 </p>
+                 <p>
+                        this is the last  article
+                 </p> `
          
-    // }
-// };
- // function createTemplate(data) {
-    //    var title = data.title;
-      //  var date = data.date;
-    //    var heading = data.heading;
-     //  var content = data.content;
-      // var htmltempalte = `
-      // <html>
-       // <head>
-         // <title>
-            //    ${title}
-       //  <meta name = "viewpoint" content="width-device-width, intial scale = 1" / >
-        // <link href> = "/ui/style.css" rel = "stylesheet" />
-         // </head>
-         // <body>
-            //     <div class = "containers" >
-               //     <div>
-                  //       <a href =" / ">Home</a>
-                 //    </div>
-                   //  <h3>
-                      //     ${heading}
-                    // </h3>
-                    // <div>
-                       //     ${date}
-                    // </div>
-                    // <div>
-                       //      ${content}    
-                    // </div>
-                // </div>
-            // </body>
-        // </html>
-    // ` ;
-       //  return htmlTemplate;
-// }
+     }
+ };
+  function createTemplate(data) {
+        var title = data.title;
+        var date = data.date;
+        var heading = data.heading;
+       var content = data.content;
+       var htmltempalte = `
+       <html>
+        <head>
+          <title>
+                ${title}
+        <meta name = "viewpoint" content="width-device-width, intial scale = 1" / >
+         <link href> = "/ui/style.css" rel = "stylesheet" />
+          </head>
+          <body>
+                 <div class = "containers" >
+                    <div>
+                         <a href =" / ">Home</a>
+                     </div>
+                     <h3>
+                           ${heading}
+                     </h3>
+                    <div>
+                            ${date}
+                    </div>
+                     <div>
+                             ${content}    
+                    </div>
+                </div>
+             </body>
+         </html>
+     ` ;
+         return htmlTemplate;
+ }
   app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
   });
@@ -96,13 +96,24 @@ app.use(morgan('combined'));
             }
       });
   });
-// app.get('/articleName', function(req,res)
-   //     {
+app.get('/articleName', function(req,res)
+        {
             //articleName == article-one
             //articles[articleName] = { } content object for article one
-     //       var articleName = req.params.articleName;
-        //    res.send(createTemplate(articles[articleName]));
-        // });
+            // SELECT * FROM article WHERE title = 'article-one'
+            pool.query(" SELECT * FROM article WHERE title = '" + req.params.articleName + " ' ", function(err,result){
+             if(err) {
+                 res.status(500).send(err.toString());
+                }   else {
+                    if(result.rows.length === 0) {
+                         res.status(404).send('Article not found');
+                    }  else {
+                        var articleData = result.rows = [0];
+                        res.send(createTemplate(articleData));
+                    }
+                }   
+            });
+         });
 
 var counter = 0;
 app.get('/counter', function (req, res) {
