399059744fdc8d699138849fca01625417ea7ba3
Tanu5 <tammanatanusha@gmail.com>
2016-11-20 20:13:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//var pool = require('pg').pool;
-//var config = {
-  //      user : 'tanu5',
-    //    databse : 'tanu5',
-      //  host : 'db-imad.hasura-app.io',
-        //port : '5432',
-        //password : process.env.DB_PASSWORD
-//};
+var pool = require('pg').pool;
+var config = {
+        user : 'tanu5',
+        databse : 'tanu5',
+        host : 'db-imad.hasura-app.io',
+        port : '5432',
+        password : process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -84,17 +84,17 @@ app.use(morgan('combined'));
   app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
   });
-  //var pool = new pool(config);
+  var pool = new pool(config);
   app.get('/test-db',function(req,res){
       //make a select request
       //return a response with results
-    //  pool.query('SELECT *FROM test',function(err,result){
-      //      if(err) {
-        //        res.status(500).send(err,toString());
-          //  } else {
-            //    res.send(JSON.stringify(result));
-            //}
-      //});
+      pool.query('SELECT *FROM test',function(err,result){
+           if(err) {
+                res.status(500).send(err,toString());
+            } else {
+                res.send(JSON.stringify(result));
+            }
+      });
   });
 // app.get('/articleName', function(req,res)
    //     {
